55cb52dbd225fca0b9a4b29428e8d43afb6660f1
Benedikt Rottstegge <benedikt@rottstegge.com>
2021-03-09 06:53:56 +0100

    Logic for Opening Artworks

--- public/js/ui.js
+++ public/js/ui.js
@@ -5,6 +5,7 @@ console.log("ui.js loaded");
 $(function(){
     let artistArea = $('.artist-area');
     let minimap = $('.minimap');
+    let overlay = $('.single-artwork-overlay');
 
 
 
@@ -56,11 +57,15 @@ $(function(){
 
     $('.artwork-thumbnails').on("mouseenter",  ".artwork-thumbnail", function(){
         // don't do anything if mouse moves after down
+        if($(overlay).hasClass('open')){ 
+           return;
+        }
         let id = $(this).attr("artwork-id");
         teaseArtwork(id);
     })
     $('.artwork-thumbnails').on("mouseleave",  ".artwork-thumbnail", function(){
         // don't do anything if mouse moves after down
+        if($(overlay).hasClass('open')){ return; }
         let id = $(this).attr("artwork-id");
         unteaseArtwork();
     })
@@ -71,17 +76,24 @@ $(function(){
         // get content and open
         populateOverlayContent(id);
         let overlay = $('.single-artwork-overlay');
-        $(overlay).addClass('open');
         $(overlay).removeClass('tease');
+        $(overlay).addClass('open');
 
-        // init gallery
-        $(overlay).find('.gallery-container').slick({
+        // init gallery, trigger transition once done
+        let galleryContainer = $('.gallery-container');
+        $(galleryContainer).on('init', function(){
+            thumbnailToGalleryTransition(id);
+        })
+        $(galleryContainer).slick({
             speed: 500,
             infinite: false,
             adaptiveHeight: true,
             variableWidth: true,
             lazyLoad: 'ondemand'
+          }).on('reInit', function(){
+              console.log("initiated slick slider");
           });
+
 
         // shrink minimap
         let oldHeight = $(minimap).outerHeight();
@@ -101,12 +113,64 @@ $(function(){
         populateOverlayContent(id);
         let overlay = $('.single-artwork-overlay');
         $(overlay).addClass('tease');
-        console.log("tease:" +  id);
+        gsap.fromTo(overlay, 
+            {transform: "translateY(10vh)"},
+            {transform: "translateY(0)", duration: 0.3}
+        );
     }
+
+
     function unteaseArtwork(){
         let overlay = $('.single-artwork-overlay');
-        $(overlay).removeClass('tease');
-        console.log("untease");
+        gsap.to(overlay, 
+            {transform: "translateY(10vh)", duration: 0.3, onComplete: removeClass}
+        );
+        function removeClass(){
+            console.log("untease");
+            $(overlay).removeClass('tease');
+            gsap.set(overlay, 
+                {transform: "translateY(0)"}
+            );
+        }
+    }
+
+
+    
+    function thumbnailToGalleryTransition(id){
+
+        let mapImage = $('.artwork-thumbnail[artwork-id='+id+']');
+        let galleryContainer = $('.gallery-container').first();
+        let mapBackground = $('.map-draggable');
+        let backgroundLeft = $(mapBackground).css('left');
+        let backgroundTop =   $(mapBackground).css('top');
+
+        // get positions relative to screen
+        mapImagePos = $(mapImage).position(window);
+        galleryContainerPos = $(galleryContainer).position(window);
+        galleryContainerHeight = $(galleryContainer).height();
+
+        let newLeft = parseInt(backgroundLeft)*-1 + galleryContainerPos.left;
+        let newTop = parseInt(backgroundTop)*-1 + galleryContainerPos.top;
+        let newWidth = galleryContainerHeight / ($(mapImage).height() / $(mapImage).width());
+
+        let tl = gsap.timeline({defaults: {duration: 1, ease: "easeIn"} });
+        tl.to(mapImage, {
+            left: newLeft, 
+            top: newTop, 
+            width: newWidth,
+            })
+            .to($(mapImage).find('.regular-image'), {
+            opacity: 1
+        }, "<")
+            .set(galleryContainer,{opacity: 1, duration: 0.3})
+            .to(mapImage, {opacity: 0, duration: 0.3});
+
+
+        console.log({newWidth});
+        console.log({mapImagePos});
+        console.log({galleryContainerPos});
+        console.log({galleryContainerHeight});
+
 
     }
 
@@ -116,8 +180,6 @@ $(function(){
     function setLayoutAlignment(){
         let brand = $('.brand');
         let singleArtworkOverlay = $('.single-artwork-overlay');
-        console.log($(brand).height());
-
 
         // layout for minimap
         let ratio = $('.map-background').outerWidth() /  $('.map-background').outerHeight();
