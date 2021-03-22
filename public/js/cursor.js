564d2ef20bcf492bff405278d5bfcf02c49323f6
Benedikt Rottstegge <benedikt@rottstegge.com>
2021-03-22 07:18:52 +0100

    Get Basic Mouse Animation to work

--- public/js/cursor.js
+++ public/js/cursor.js
@@ -1,46 +1,65 @@
 
 $(function(){
     console.log("cursor.js");
-     let cursorContainer = $('.cursor-container');
-     let cursorSVG = $('.cursor-svg');
+    let cursorContainer = $('.cursor-container');
+    let cursorSVG = $('.cursor-svg');
+    let defaultPath = $('path#default');
+    
+    // make object follow mouse
     $(window).on('mousemove', function(event){
-        console.log(event.pageX);
         $(cursorContainer).css({
             'left': event.pageX,
             'top': event.pageY
         });
     })
+    
+    
+    
 
-
-
+    // Mutation Observer
     // different mouse states are handled via mutation observer on the "cursor state attribute"
     function attributeChanged(mutationList) {
-        mutationList.forEach(function(mutation) {
-          switch(mutation.type) {
-            case "attributes":
-              notifyUser("Attribute name " + mutation.attributeName +
-                  " changed to " + mutation.target[mutation.attributeName] +
-                  " (was " + mutation.oldValue + ")");
-              break;
-          }
-        });
-      }
-      
-      var targetNode = $(cursorContainer)[0];
-      
-
-      // cursor states can be crosshair, open, close, arrow-right, arrow-left
-      var observer = new MutationObserver(attributeChanged);
-      observer.observe(targetNode, {
+        let mutation = mutationList[0];
+        let newValue = mutation.target.attributes["cursor-state"].nodeValue;
+        let oldValue = mutation.oldValue;
+        animateCursor(oldValue, newValue);
+    }
+    var targetNode = $(cursorContainer)[0];
+    // cursor states can be default, open, close, arrow-right, arrow-left
+    var observer = new MutationObserver(attributeChanged);
+    observer.observe(targetNode, {
         attributes: true,
         attributeOldValue: true,
         attributeFilter: ["cursor-state"]
-      });
+    });
+    
 
+    
+    
+    
+    
+    // Welll
+    function setMouseState(state){
+        $(cursorContainer).attr("cursor-state", state);
+    }
+    
+    function animateCursor(oldPath, newPath){
+        gsap.to(defaultPath, {morphSVG: {shape: `#${newPath}`, shapeIndex:1}, duration: 0.3})
+    }
+    
+    $('.gallery-container').on("mouseenter", ".slick-prev", function(){
+        setMouseState("arrowleft");
+    })
+    $('.gallery-container').on("mouseleave", ".slick-prev", function(){
+        setMouseState("close");
+    });
+    
+    $('.gallery-container').on("mouseenter", ".slick-next", function(){
+        setMouseState("arrowright");
+    });
+    $('.gallery-container').on("mouseleave", ".slick-next", function(){
+        setMouseState("close");
+    });
 
-      function animateMouseFromTo(oldState, newState){
-
-      }
-
-
+    
 })
